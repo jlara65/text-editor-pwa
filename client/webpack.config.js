00a476f0485bcad570e30204ae8fbf05686c7fa8
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-11-17 23:11:39 -0600

    added CSS loaders and babel to webpack in clicent/webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,19 +11,30 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-      
-    ],
+    plugins: [],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [['@babel/preset-env', { targets: 'defaults' }]],
+            },
+          },
+        },
       ],
     },
   };
